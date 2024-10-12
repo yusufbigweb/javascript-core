bb200e86237d912be556bdfc7d83864d5ee06668
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-12 20:33:20 +0000

    Update

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Yusuf Shaikh")
+//script
+const btnAddName = document.querySelector("#addName");
+const btnAddSure = document.querySelector("#addSure");
+const tableShow = document.getElementById("tableList");
+
+btnAddName.addEventListener("click", function () {
+  const newRow = document.createElement("tr");
+  const userName = prompt("Enter Your Name Only:");
+
+  newRow.innerHTML = `
+  
+  <td><ul><li>${userName}</li></ul></td>
+
+  `;
+  tableShow.appendChild(newRow);
+
+  btnAddSure.addEventListener("click", () => {
+    const newRow = document.createElement("tr");
+    const userSure = prompt("Enter Your Sure Name Only:");
+
+    newRow.innerHTML = `
+    
+    <td><ul><li>${userSure}</li></ul></td>
+  
+    `;
+    tableShow.appendChild(newRow);
+  });
+});
