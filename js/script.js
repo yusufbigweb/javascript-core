0f5f1b5e0c9a33b988715a5fa1d55681bf95d1a2
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-14 11:04:46 +0000

    Update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Varible
+//Data Types
+//Convertion Data
+//Control Flow
+//Function
+//Array
+//Object
+//DOM - Document Object Modal JavaScript
+//Work JavaScript Behind
+//Work Call Stack
+//async and sync in javascript work
+//API
+//OOP
