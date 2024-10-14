0cf70acd15a1fc0d93dee5c1902676bcd1940bba
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-14 11:28:03 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//  --- What I Learn
+
 //Varible
 //Data Types
 //Convertion Data
@@ -11,3 +13,40 @@
 //async and sync in javascript work
 //API
 //OOP
+//Promise, try, catch, error
+
+// --- What I Return Learn this Topics are:
+
+// ====== Basic
+
+//  1. String Manipulation - (length, slice(), substring(), replace(), etc.)
+//  2. Array Methods Like - (push(), pop(), shift(), unshift(), map(), filter(), etc.) 
+
+// ====== Control Flow
+
+//  1. if else, else if, switch
+//  2. Loop - (for, while, do...while)
+//  3. break and continue statements
+
+// ====== Function
+
+// 1. Higher-order Functions (functions as arguments or returned from another function)
+// 2. Closures and Scoping
+
+// ===== Objects
+
+// 1. Object Creation (Object Literals, new Object())
+// 2. Accessing and Modifying Object Properties
+// 3. this keyword
+// 4. Object Methods
+// 5. Object Destructuring
+// 6. JSON (parsing and stringifying)
+
+// ===== DOM Manipulation
+
+//  1. Event Listeners (click, input, keypress)
+//  2. Creating and Appending Elements (createElement(), appendChild())
+
+// ===== DOM Manipulation
+
+
