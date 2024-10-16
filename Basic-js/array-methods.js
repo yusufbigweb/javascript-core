c035032ddc00fe5372b25a7f71cb74eb3c545f2f
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-16 06:40:32 +0000

    Update

--- Basic-js/array-methods.js
+++ Basic-js/array-methods.js
@@ -13,5 +13,23 @@ const arrNumber = [0,1,2,3,4,5,6,7,8]
 arrNumber.forEach(numberArr)
 
 function numberArr(item, index){
-    console.log(item);
+    // console.log(item);
 }
+
+
+// ====
+
+
+const letter = ["a","b","c","d","s","f","g","s"]
+
+let count = {}
+
+letter.forEach((item)=>{
+    if(count[item]){
+        count[item]++
+    }else{
+        count[item]+1
+    }
+})
+
+console.log(count)
