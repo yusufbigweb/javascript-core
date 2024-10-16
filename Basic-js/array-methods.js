08f801ef91a3237526f52bf1ddf5dc53dc36c1f5
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-16 05:23:44 +0000

    Update

---
+++ Basic-js/array-methods.js
@@ -0,0 +1,17 @@
+// Array Methods
+
+const numberArray = [1,2,3,4,5,6,7,8,9,10];
+
+numberArray.forEach(function(item, index, arr){
+    // console.log(item)
+})
+
+
+
+const arrNumber = [0,1,2,3,4,5,6,7,8]
+
+arrNumber.forEach(numberArr)
+
+function numberArr(item, index){
+    console.log(item);
+}
