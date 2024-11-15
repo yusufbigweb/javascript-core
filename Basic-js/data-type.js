f7007abbc69f457d6c76d9c22ec4906d3d12c7db
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-15 15:25:10 +0000

    Update by Anuj Bhaiya

---
+++ Basic-js/data-type.js
@@ -0,0 +1,36 @@
+// Data Type (Primitive) here:
+
+// 1 - Number
+// 2 - String
+// 3 - Boolean
+// 4 - Undefind
+// 5 - Null
+
+
+
+// Primitive - data type
+
+const number = 2121;
+const string = "String";
+const booleanFalse = false;
+const booleanTrue = true;
+const undefind = undefined;
+const DataTypeNull = null;
+console.log(number);
+console.log(string);
+console.log(booleanFalse);
+console.log(booleanTrue);
+console.log(undefind);
+console.log(DataTypeNull);
+
+
+
+// Data Conversion()
+// console.log(typeof number.toString()) // string
+// -------
+// const convertStringToNumber = Number("1212")
+// let convertStringToNumber = "1212" // this is simple data conversion
+// console.log(typeof convertStringToNumber)
+
+
+
